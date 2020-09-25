ded015324f127ed94c821c9f47941485168f9484
Chris L <c.aventine@gmail.com>
2020-09-25 13:15:43 -0500

    Complete Step 3.1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Widget
+from django.views.generic.edit import DeleteView
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html', {'widgets': Widget.objects.all()})
+
